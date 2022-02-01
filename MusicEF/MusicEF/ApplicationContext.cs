6584d6f8c8522ce82a9346e027c929e84d6fb7da
kbodyan <kbodyan@gmail.com>
2022-02-01 12:24:56 +0200

    empty_progect

---
+++ MusicEF/MusicEF/ApplicationContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using FirstEF.Entities;
+using FirstEF.EntitiesConfiguration;
+
+namespace MusicEF
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Office> Offices { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Title> Titles { get; set; }
+        public DbSet<EmployeeProject> EmployeeProjects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=CompanyDB;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new OfficeConfiguration());
+            modelBuilder.ApplyConfiguration(new TitleConfiguration());
+            modelBuilder.ApplyConfiguration(new ProjectConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeProjectConfiguration());
+        }
+    }
+}
